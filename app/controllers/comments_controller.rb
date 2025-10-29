a09c67782b18e7b2d3c07bf651122e3117686f55
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 10:46:36 -0600

    style: use rubocop to fix style problems

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
@@ -6,7 +8,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
